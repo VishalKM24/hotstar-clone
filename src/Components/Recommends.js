07ebf48c249deb22ce357c43642ad5be0797b7b1
Vishalkumar Mandal <vkmandal2608@gmail.com>
2021-04-27 00:10:48 +0530

    Firebase and Slider

---
+++ src/Components/Recommends.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Recommends = (props) => {
+    return (
+        <div>
+            <h4>
+                Recommended For you
+            </h4>
+        </div>
+    );
+}
+
+export default Recommends;
